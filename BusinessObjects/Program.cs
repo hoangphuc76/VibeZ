4fef187f457bfc81e3f56257668f039d9c40e416
HoangPhuc <vacpro66@gmail.com>
2024-09-16 22:28:07 +0700

    Fisrt

---
+++ BusinessObjects/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BusinessObjects
+{
+    public class VibeZDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Follow> Follows { get; set; }
+        public virtual DbSet<BlockedArtist> BlockedArtists { get; set; }
+        public virtual DbSet<Library> Libraries { get; set; }
+        //public virtual DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.
+                GetConnectionString("VibeZDB"));
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ BusinessObjects/VibeZDbContext.cs
@@ -0,0 +1,37 @@
+﻿    using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BusinessObjects
+{
+    public class VibeZDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Follow> Follows { get; set; }
+        public virtual DbSet<BlockedArtist> BlockedArtists { get; set; }
+        public virtual DbSet<Feature> Features { get; set; }
+        public virtual DbSet<Library> Libraries { get; set; }
+        public virtual DbSet<Package> Packages { get; set; }
+        public virtual DbSet<Package_features> P_features { get; set; }
+        public virtual DbSet<Payment> Payments { get; set; }
+        public virtual DbSet<User_package> U_packages { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+            IConfigurationRoot configuration = builder.Build();
+            optionsBuilder.UseSqlServer(configuration.
+                GetConnectionString("VibeZDB"));
+
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
